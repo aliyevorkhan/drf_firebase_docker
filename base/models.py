13bd60eefdf1dfd722b26b39f7819cf0b59bd8df
Orkhan Aliyev <orkhan.aliyev@ayvos.com>
2022-05-29 19:46:35 +0400

    base app deleted and all model credentials moved to firebase_auth app

--- base/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Item(models.Model):
-    name = models.CharField(max_length=100)
-    created = models.DateTimeField(auto_now_add=True) 
